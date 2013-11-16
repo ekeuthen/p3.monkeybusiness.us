4f5652bfcb6f9a34aae649d942bac3493790a894
Emily Keuthen <ekeuthen@gmail.com>
2013-11-16 17:24:58 -0500

    fixing count issues

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-	// accordion functionality
+	// display word tiles in accordion
     $( '#accordion' ).accordion({
     	collapsible: true,
     	active: false
     });
 
-    // droppable functionality
+    // allow items to be cloned and dropped
     $(".droppable").droppable({
         accept: '.draggable', //only objects tagged as draggable will be accepted
         drop: function(event, ui) {
@@ -21,65 +21,67 @@ $(document).ready(function() {
             else { //when word is subsequently moved between verses, update DOM
                 $(this).append(ui.draggable);
             }
-
-            //for each verse, iterate through to update counter
-            var $poem = $('#haiku');
-            var $verses = $poem.children('div').find('.droppable');
-            $verses.each(function(i) {
-
-                //determine how much to increment counter by looping through line's children words
-                console.log('going through verse loop!');
-                console.log($(this));
-                var $children = $(this).children();
-                var $totalIncrement = 0;
-                $children.each(function(i) { 
-                var $increment = 0;
-                    if ($(this).hasClass('one')) {
-                        $increment = 1;
-                    }
-                    else if ($(this).hasClass('two')) {
-                        $increment = 2;
-                    }
-                    else if ($(this).hasClass('three')) {
-                        $increment = 3;
-                    }
-                    else if ($(this).hasClass('four')) {
-                        $increment = 4;
-                    }
-                    else if ($(this).hasClass('five')) {
-                        $increment = 5;
-                    }
-                    else if ($(this).hasClass('six')) {
-                        $increment = 6;
-                    }
-                    else if ($(this).hasClass('seven')) {
-                        $increment = 7;
-                    }
-                    $totalIncrement += $increment;
-                });
-
-                // increment counter based on totalIncrement
-                var $dropspot = $(this).parent().next('span'); 
-                var $value = $totalIncrement;
-                $($dropspot).text($value);
-
-                //check if verse has enough syllables for given line; if so, display checkmark
-                $verse = $dropspot.prev('div');
-                if ((parseInt($($dropspot).text()) == 7) && ($verse.is("#line2"))) { 
-                    $dropspot.next('img').show();
-                }
-                else if ((parseInt($($dropspot).text()) == 5) && (($verse.is("#line1"))||($verse.is("#line3")))) {
-                    $dropspot.next('img').show();
-                }
-                else {
-                    $dropspot.next('img').hide();
-                }
-
-            });
+            updateCounters();
         }
     });
 
-    // draggable functionality
+    // iterate through each verse's words to determine count
+    function updateCounters() {
+        var $poem = $('#haiku');
+        var $verses = $poem.children('div').find('.droppable');
+        $verses.each(function(i) {
+
+            //determine how much to increment counter by looping through line's children words
+            console.log('going through verse loop!');
+            console.log($(this));
+            var $children = $(this).children();
+            var $totalIncrement = 0;
+            $children.each(function(i) { 
+            var $increment = 0;
+                if ($(this).hasClass('one')) {
+                    $increment = 1;
+                }
+                else if ($(this).hasClass('two')) {
+                    $increment = 2;
+                }
+                else if ($(this).hasClass('three')) {
+                    $increment = 3;
+                }
+                else if ($(this).hasClass('four')) {
+                    $increment = 4;
+                }
+                else if ($(this).hasClass('five')) {
+                    $increment = 5;
+                }
+                else if ($(this).hasClass('six')) {
+                    $increment = 6;
+                }
+                else if ($(this).hasClass('seven')) {
+                    $increment = 7;
+                }
+                $totalIncrement += $increment;
+            });
+
+            // increment counter based on totalIncrement
+            var $dropspot = $(this).parent().next('span'); 
+            var $value = $totalIncrement;
+            $($dropspot).text($value);
+
+            // hide / display checkmark indicating complete verse
+            $verse = $dropspot.prev('div');
+            if ((parseInt($($dropspot).text()) == 7) && ($verse.is("#line2"))) { 
+                $dropspot.next('img').show();
+            }
+            else if ((parseInt($($dropspot).text()) == 5) && (($verse.is("#line1"))||($verse.is("#line3")))) {
+                $dropspot.next('img').show();
+            }
+            else {
+                $dropspot.next('img').hide();
+            }
+        });
+    }
+
+    // allow items to be dragged and cloned
     $(".draggable").draggable({
         cursor: 'pointer',
         snap: ".droppable,.draggable",
@@ -87,16 +89,17 @@ $(document).ready(function() {
 		revert:"invalid" //if object is not dropped appropriately, revert to original position
     });
 
-	// clear screen functionality
+	// clear screen 
 	$('#clear').click(function() {
 		location.reload();
 	});
 
-	// delete words functionality
+	// delete individual word tiles
 	$('#trash').droppable({
         accept: '.droppable2',
         drop: function(event, ui) {
             $(ui.draggable).remove();
+            updateCounters();
         }
     });
 
