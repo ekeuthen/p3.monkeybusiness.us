41252fff2b8692ed42db0ef12110725040e10c94
Emily Keuthen <ekeuthen@gmail.com>
2013-11-19 22:17:56 -0500

    refactoring javascript variable names

--- javascript/p3.js
+++ javascript/p3.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
     $(".droppable").droppable({
         accept: '.draggable', //only objects tagged as draggable will be accepted
         drop: function(event, ui) {
-            var $clone = ui.helper.clone(); 
-            if (!$clone.is('.inside-droppable')) { //when word is initially dropped in verse
-                $(this).append($clone.addClass('inside-droppable').draggable({
+            var clone = ui.helper.clone(); 
+            if (!clone.is('.inside-droppable')) { //when word is initially dropped in verse
+                $(this).append(clone.addClass('inside-droppable').draggable({
 					revert:"invalid", //clone reverts to original position if not dropped approprately
                     snap: ".droppable,.draggable"
                 }));
-                $clone.addClass("droppable2");
+                clone.addClass("droppable2");
             }
             else { //when word is subsequently moved between verses, update DOM
                 $(this).append(ui.draggable);
@@ -27,54 +27,54 @@ $(document).ready(function() {
 
     // iterate through each verse's words to determine count
     function updateCounters() {
-        var $poem = $('#haiku');
-        var $verses = $poem.children('div').find('.droppable');
-        $verses.each(function(i) {
+        var poem = $('#haiku');
+        var verses = poem.children('div').find('.droppable');
+        verses.each(function(i) {
 
             //determine how much to increment counter by looping through line's children words
-            var $children = $(this).children();
-            var $totalIncrement = 0;
-            $children.each(function(i) { 
-            var $increment = 0;
+            var children = $(this).children();
+            var totalIncrement = 0;
+            children.each(function(i) { 
+            var increment = 0;
                 if ($(this).hasClass('one')) {
-                    $increment = 1;
+                    increment = 1;
                 }
                 else if ($(this).hasClass('two')) {
-                    $increment = 2;
+                    increment = 2;
                 }
                 else if ($(this).hasClass('three')) {
-                    $increment = 3;
+                    increment = 3;
                 }
                 else if ($(this).hasClass('four')) {
-                    $increment = 4;
+                    increment = 4;
                 }
                 else if ($(this).hasClass('five')) {
-                    $increment = 5;
+                    increment = 5;
                 }
                 else if ($(this).hasClass('six')) {
-                    $increment = 6;
+                    increment = 6;
                 }
                 else if ($(this).hasClass('seven')) {
-                    $increment = 7;
+                    increment = 7;
                 }
-                $totalIncrement += $increment;
+                totalIncrement += increment;
             });
 
             // increment counter based on totalIncrement
-            var $dropspot = $(this).parent().next('span'); 
-            var $value = $totalIncrement;
-            $($dropspot).text($value);
+            var dropspot = $(this).parent().next('span'); 
+            var value = totalIncrement;
+            $(dropspot).text(value);
 
             // hide / display checkmark indicating complete verse
-            $verse = $dropspot.prev('div');
-            if ((parseInt($($dropspot).text()) == 7) && ($verse.is("#line2"))) { 
-                $dropspot.next('img').show();
+            var verse = dropspot.prev('div');
+            if ((parseInt($(dropspot).text()) == 7) && (verse.is("#line2"))) { 
+                dropspot.next('img').show();
             }
-            else if ((parseInt($($dropspot).text()) == 5) && (($verse.is("#line1"))||($verse.is("#line3")))) {
-                $dropspot.next('img').show();
+            else if ((parseInt($(dropspot).text()) == 5) && ((verse.is("#line1"))||(verse.is("#line3")))) {
+                dropspot.next('img').show();
             }
             else {
-                $dropspot.next('img').hide();
+                dropspot.next('img').hide();
             }
         });
     }
