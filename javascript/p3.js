1331e103b4ff3869f54be95154cd783bb7c2be1c
Emily Keuthen <ekeuthen@gmail.com>
2013-11-25 20:21:54 -0500

    modifing code formatting

--- javascript/p3.js
+++ javascript/p3.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
 
     // display word tiles in accordion will collapsed panels
-    $( '#accordion' ).accordion({
+    $("#accordion").accordion({
         collapsible: true,
         active: false
     });
 
     // allow items to be cloned and dropped
     $(".droppable").droppable({
-        accept: '.draggable', //only objects tagged as draggable will be accepted
+        accept: ".draggable", //only objects tagged as draggable will be accepted
         drop: function(event, ui) {  //upon drop, set clone's properties
-            var clone = ui.helper.clone(); 
-            if (!clone.is('.inside-droppable')) { //when word is initially dropped in verse
-                $(this).append(clone.addClass('inside-droppable').draggable({
+        var clone = ui.helper.clone(); 
+            if (!clone.is(".inside-droppable")) { //when word is initially dropped in verse
+                $(this).append(clone.addClass("inside-droppable").draggable({
                     revert:"invalid", 
                     snap: ".droppable,.draggable"
                 }));
                 clone.addClass("droppable2");
-            }
-            else { //when word is subsequently moved between verses, update DOM
+            } else { //when word is subsequently moved between verses, update DOM
                 $(this).append(ui.draggable);
             }
             determineSyllables();
@@ -27,32 +26,32 @@ $(document).ready(function() {
 
     // iterate through each verse's words to determine count
     function determineSyllables() {
-        var poem = $('#haiku');
-        var verses = poem.children('div').find('.droppable');
+        var poem = $("#haiku");
+        var verses = poem.children("div").find(".droppable");
         verses.each(function() {
             //determine how much to increment counter by looping through line's children words
             var children = $(this).children();
             var totalIncrement = 0;
             children.each(function() { 
                 var increment = 0;
-                var classNameList = $(this).attr('class');
+                var classNameList = $(this).attr("class");
                 //turn classNameList string into an array of classes
                 var className = classNameList.split(" ");
                 for(i = 0; i < className.length; i++) {
                     switch (className[i]) {
-                        case 'one': increment = 1; break;
-                        case 'two': increment = 2; break;
-                        case 'three': increment = 3; break;
-                        case 'four': increment = 4; break;
-                        case 'five': increment = 5; break;
-                        case 'six': increment = 6; break;
-                        case 'seven': increment = 7; break;
+                        case "one": increment = 1; break;
+                        case "two": increment = 2; break;
+                        case "three": increment = 3; break;
+                        case "four": increment = 4; break;
+                        case "five": increment = 5; break;
+                        case "six": increment = 6; break;
+                        case "seven": increment = 7; break;
                         default: increment = 0;
                     }
                     totalIncrement += increment;
                 }
             });
-            var dropspot = $(this).parent().next('span'); 
+            var dropspot = $(this).parent().next("span"); 
             updateCounter(dropspot, totalIncrement);
         });
     }
@@ -61,35 +60,35 @@ $(document).ready(function() {
     // determine if success checkmark should be displayed based on haiku syllable rules
     function updateCounter(dropspot, totalIncrement) {
         $(dropspot).text(totalIncrement);
-        var verse = dropspot.prev('div');
-        if ((parseInt($(dropspot).text()) == 7) && (verse.is("#line2"))) { 
-            dropspot.next('img').show();
-        }
-        else if ((parseInt($(dropspot).text()) == 5) && ((verse.is("#line1"))||(verse.is("#line3")))) {
-            dropspot.next('img').show();
-        }
-        else {
-            dropspot.next('img').hide();
+        var verse = dropspot.prev("div");
+        if ((parseInt($(dropspot).text()) == 7) && 
+            (verse.is("#line2"))) { 
+            dropspot.next("img").show();
+        } else if ((parseInt($(dropspot).text()) == 5) && 
+            ((verse.is("#line1"))||(verse.is("#line3")))) {
+            dropspot.next("img").show();
+        } else {
+            dropspot.next("img").hide();
         }
     }
 
     // allow items to be dragged and cloned; set draggable properties
     $(".draggable").draggable({
-        cursor: 'pointer',
+        cursor: "pointer",
         snap: ".droppable,.draggable",
-        helper: 'clone', 
+        helper: "clone", 
         revert:"invalid" 
     });
 
-        // clear screen 
-        $('#clear').click(function() {
-            location.reload();
-        });
+    // clear screen 
+    $("#clear").click(function() {
+        location.reload();
+    });
 
-        // delete individual word tiles
-        // only tiles already in poem may be deleted
-        $('#trash').droppable({
-        accept: '.droppable2',
+    // delete individual word tiles
+    // only tiles already in poem may be deleted
+    $("#trash").droppable({
+        accept: ".droppable2",
         drop: function(event, ui) {
             $(ui.draggable).remove();
             determineSyllables();
